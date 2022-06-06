5cd9e0e243b35821e9fed3927afcb06378958552
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2022-06-06 21:47:54 +0100

    Create basic form with validation for creating a team.

---
+++ FootballAppSolution/FootballAppUI/Controllers/SetupController.cs
@@ -0,0 +1,34 @@
+﻿using FootballAppUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballAppUI.Controllers
+{
+    public class SetupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Teams()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateTeam(TeamViewModel team)
+        {
+            if (ModelState.IsValid)
+            {
+                return new StatusCodeResult(StatusCodes.Status200OK);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ FootballAppSolution/FootballAppUI/Models/TeamViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootballAppUI.Models
+{
+    public class TeamViewModel
+    {
+
+        [Display(Name = "Team")]
+        [Required(ErrorMessage = "Team name is required")]
+        public string Name { get; set; }
+        
+        [Display(Name = "Venue")]
+        public string VenueName { get; set; }
+
+        [Display(Name = "Code")]
+        [Required(ErrorMessage = "Code is required")]
+        public string Code { get; set; }
+
+        public TeamViewModel()
+        {
+
+        }
+
+        public TeamViewModel(string name, string venueName, string code)
+        {
+            Name = name;
+            VenueName = venueName;
+            Code = code;
+        }    
+    }
+}
