07a8c198e7078f0388923d807d27b771a2594644
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2022-06-07 21:11:31 +0100

    Add initialData script.

---
+++ FootballAppSolution/Infastructure/InitialData.sql
@@ -0,0 +1,24 @@
+DELETE FROM Teams;
+
+INSERT INTO Teams([Name],Code, HomeGround, Capacity)
+VALUES 
+('Arsenal', 'ARS','Emirates Stadium', 40000),
+('Aston Villa', 'ASV','Villa Park', 40000),
+('Brentford', 'BRE','Brentford Community Stadium', 40000),
+('Brighton & Hove Albion', 'BRI','Amex Stadium', 40000),
+('Burnley', 'BUR','Turf Moor', 40000),
+('Chelsea', 'CHE','Stamford Bridge', 40000),
+('Crystal Palace', 'CRY','Selhurst Park', 40000),
+('Everton', 'EVE','Goodison Park', 40000),
+('Leeds United', 'LEE','Elland Road', 40000),
+('Leicester', 'LEI','King Power Stadium', 40000),
+('Liverpool', 'LIV','Anfield', 40000),
+('Manchester City', 'MCI','The Etihad Stadium', 40000),
+('Manchester United', 'MUN','Old Trafford', 40000),
+('Newcastle United', 'NEW','St James Park', 40000),
+('Norwich City', 'NOR','Carrow Road', 40000),
+('Southampton', 'SOU','St Mary Stadium', 40000),
+('Tottenham Hotspur', 'TOT','Tottenham Hotspur Stadium', 40000),
+('Watford', 'WAT','Vicarage Road', 40000),
+('West Ham United', 'WHU','London Stadium', 40000),
+('Wolerhampton Wanderers', 'WOL','The Melineux', 40000);
