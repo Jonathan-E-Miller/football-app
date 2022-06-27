169ae3c1dec73ffb0433b5326429fe7abad06fcb
Jonathan-E-Miller <jonnymiller1992@hotmail.co.uk>
2022-06-27 21:49:33 +0100

    Prepare create season wizard

---
+++ FootballAppSolution/FootballAppUI/Models/SeasonViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FootballAppUI.Models
+{
+    public class SeasonViewModel
+    {
+        public IEnumerable<LeagueViewModel> Leagues { get; set; }
+        public Dictionary<TeamViewModel, bool> SelectedTeams { get; set; }
+    }
+}
